78cf1c6fb71b2d9a46698099f2e3bc13b32bcd2e
sujeet <sujeetmalagundi999@gmail.com>
2026-04-16 01:06:26 +0530

    url

--- app.js
+++ app.js
@@ -85,6 +85,11 @@ app.use((req,res,next)=>{
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews" ,reviewRouter);
+
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+})
+
 app.use("/", userRouter);
 
 
